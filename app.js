93a161e1c335cda5c8c249b524f82d01ea81109f
Julian <142232902+JuuhAndres@users.noreply.github.com>
2024-12-03 01:44:23 -0300

    Commit Initial

---
+++ app.js
@@ -0,0 +1,51 @@
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+
+let carouselDom = document.querySelector('.carousel');
+let sliderDom = document.querySelector('.carousel .list');
+let thumbnailBorderDom=document.querySelector('.carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom= document.querySelector('.carousel .time');
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning= 3000;
+let timeAutoNext=7000;
+nextDom.onclick = function(){
+    showSlider('next');
+
+}
+prevDom.onclick = function(){
+    showSlider('prev');
+
+}
+let runTimeOut;
+let nextRunAuto = setTimeout(()=>{
+    next.click()
+},timeAutoNext);
+
+function showSlider(type){
+    let sliderItemDom = sliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+
+    if(type === 'next'){
+        sliderDom.appendChild(sliderItemDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add('next');
+    }else{
+        sliderDom.appendChild(sliderItemDom[sliderItemDom.length-1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length-1]);
+        carouselDom.classList.add('prev');
+    }
+
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(()=>{
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning)
+
+    clearTimeout(runAutoRun);
+    runAutoRun = setTimeout(()=>{
+        next.click()
+    },timeAutoNext);
+   
+}
